7f0f1c8e88efc5a978d67100eebb37604db802c8
Rama <45132664+rama42111869@users.noreply.github.com>
2022-09-08 16:59:33 -0300

    Implementacion de body

--- Pagina/angular/src/app/app.module.ts
+++ Pagina/angular/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgImageSliderModule } from 'ng-image-slider';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgImageSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
