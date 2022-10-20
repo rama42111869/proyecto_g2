4774c911185e71db2f573201ecbb9daa8a0fbf61
Lucas <lucaspalomodev@gmail.com>
2022-10-20 16:34:14 -0300

    Revert "Shopping Cart"
    This reverts commit fccc940c05e53df08b9e2d2ead40b9a38770e806.

--- ecommerce/src/main/java/com/ecommerce/ecommerce/DBModel/ProductJPA.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/DBModel/ProductJPA.java
@@ -4,7 +4,14 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -41,7 +48,7 @@ public class ProductJPA {
     @OneToMany(mappedBy = "product")
     @JsonManagedReference
     private List<PurchaseJPA> purchases;
-
+    
     public ProductJPA(String name, String description, double price, String images, String brand) {
         this.name = name;
         this.description = description;
